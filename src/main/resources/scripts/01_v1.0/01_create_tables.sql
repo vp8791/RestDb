33ec3aa089d881047b9b94ec01a417700b758172
vp8791 <neetha.po@gmail.com>
2018-08-19 10:58:10 +0100

    Sql

--- src/main/resources/scripts/01_v1.0/01_create_tables.sql
+++ src/main/resources/scripts/01_v1.0/01_create_tables.sql
@@ -1,15 +1,14 @@
---------------------------------------------------------
---  File created - DB  4.0    
---------------------------------------------------------
+
 
   CREATE TABLE  "USERS" 
-   (	"ID" NUMBER(19,0) NOT NULL ENABLE, 
+   (	
+    "ID" NUMBER(19,0) NOT NULL ENABLE, 
 	"CREATEDATE" TIMESTAMP (6), 
-	"EMAIL" VARCHAR2(255 CHAR), 
-	"FIRSTNAME" VARCHAR2(255 CHAR), 
-	"LASTNAME" VARCHAR2(255 CHAR), 
-	"PASSWORD" VARCHAR2(255 CHAR), 
-	"SEX" VARCHAR2(255 CHAR), 
+	"EMAIL" VARCHAR2(255), 
+	"FIRSTNAME" VARCHAR2(255), 
+	"LASTNAME" VARCHAR2(255), 
+	"PASSWORD" VARCHAR2(255), 
+	"SEX" VARCHAR2(255), 
 	"UPDATEDDATE" TIMESTAMP (6), 
 	 PRIMARY KEY ("ID")
    );
--- src/main/resources/scripts/01_v1.0/02_create_sequences.sql
+++ src/main/resources/scripts/01_v1.0/02_create_sequences.sql
@@ -1,6 +1,3 @@
---------------------------------------------------------
---  File created - Release  4.0    
---------------------------------------------------------
 
 
 CREATE SEQUENCE REST_SEQUENCE_GENERATOR INCREMENT BY 1 MAXVALUE 9999999999 MINVALUE 1 CACHE 20;
